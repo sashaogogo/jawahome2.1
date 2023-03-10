53f88d0f49bdd8247f2d3b998658fa10895e38cd
sashaogogo <alexgameover1@gmail.com>
2023-03-10 18:45:24 +0300

    cheange src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         //Стоимость билета
-        int a = 5000;
+        int ticketPrice = 5000;
         //Количество рублей для одной бонусной мили
-        int b = 20;
+        int forOneMile = 20;
         //Расчитываем количество бонусных милей
-        int c = a / b;
+        int bonusMiles = ticketPrice / forOneMile;
         //Выводим количество бонусных миль на экран
-        System.out.println("Количество бонусных миль = " + c);
+        System.out.println("Количество бонусных миль = " + bonusMiles);
     }
 }
